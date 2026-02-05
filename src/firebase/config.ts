2de4325d030245e90fa034328dc7a40a8ca151a5
Priyanshu Production <priyanshuproduction280@gmail.com>
2026-02-05 14:59:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1245027255-79c78",
+  "appId": "1:1045897290123:web:c0a647d6f0c6dd9f8e8f8e",
+  "apiKey": "AIzaSyAmvP65I2xfZKJ23wBO2rtoH27_JqFY4fQ",
+  "authDomain": "studio-1245027255-79c78.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1045897290123"
+};
